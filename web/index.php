a8be7bf535d4cb955a6f4fc00b3d48966ab2ea40
Bruno_ANDRIEU <andrieu.bruno@gmail.com>
2020-04-07 15:49:48 +0200

    creation gitignore netbeans

--- web/index.php
+++ web/index.php
@@ -31,6 +31,16 @@ switch ($loc) {
             break;
         }
 }
+?>
+<script>  
+document.writeln("<br/>screen.width: "+screen.width);  
+document.writeln("<br/>screen.height: "+screen.height);  
+document.writeln("<br/>screen.availWidth: "+screen.availWidth);  
+document.writeln("<br/>screen.availHeight: "+screen.availHeight);  
+document.writeln("<br/>screen.colorDepth: "+screen.colorDepth);  
+document.writeln("<br/>screen.pixelDepth: "+screen.pixelDepth); 
+document.writeln("<br/>navigator.appName is: "+screen.pixelDepth); 
+</script>  
 
 
 
